a3f6021404661549deb35bcedc9c58712ce53ddc
GuaHsu <guaswork@gmail.com>
2019-02-01 22:24:16 +0800

    Modify Layout to Class Component from functional component, Modify SideDrawer component style for show/close

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,27 @@ import React from 'react'
 
 import classes from './SideDrawer.module.css'
 import Logo from '../../Logo/Logo'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 import NavigationItems from '../NavigationItems/NavigationItems'
 
 const sideDrawer = props => {
-  //
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <Logo customClass={classes.Logo} />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <>
+      <Backdrop
+        show={props.open}
+        clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <Logo customClass={classes.Logo} />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </>
   )
 }
 
