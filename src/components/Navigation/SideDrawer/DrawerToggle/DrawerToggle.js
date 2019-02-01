5b9a953faa10b08af40d6e8c0c8bf3721a0cf165
GuaHsu <guaswork@gmail.com>
2019-02-01 23:17:47 +0800

    Add DrawerToggle button style

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import classes from './DrawerToggle.module.css'
+
 const drawerToggle = props => (
-  <div onClick={props.clicked}>
-    <strong>MENU</strong>
+  <div
+    className={classes.DrawerToggle}
+    onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
   </div>
 )
 
