6e9b9a341b904b2f9fbe95e51b9d17838fecc821
GuaHsu <guaswork@gmail.com>
2019-02-08 23:07:45 +0800

    Outputting order data to Orders component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import classes from './Order.module.css'
 
-const order = props => (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
-  </div>
-)
+const order = props => {
+  const ingredients = Object.keys(props.ingredients)
+    .map(key => {
+      return {
+        name: key,
+        amount: props.ingredients[key]
+      }
+    })
+    .map(ingredient => (
+      <span
+        className={classes.Ingredient}
+        key={ingredient.name} >
+        {ingredient.name}({ingredient.amount})
+      </span>
+    ))
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredients}</p>
+      <p>Price: <strong>{parseInt(props.price, 10).toFixed(2)}</strong></p>
+    </div>
+  )
+}
 
 export default order
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import axios from '../../axios-orders'
 
 import Order from '../../components/Order/Order'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount () {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchOrders = Object.keys(res.data).map(key => {
+          return { id: key, ...res.data[key] }
+        })
+        console.log(fetchOrders)
+        this.setState({
+          loading: false,
+          orders: fetchOrders
+        })
+      })
+      .catch(_ =>
+        this.setState({ loading: false })
+      )
+  }
+
   render () {
+    const orders = this.state.orders.map(order => (
+      <Order
+        key={order.id}
+        ingredients={order.ingredients}
+        price={order.price} />
+    ))
     return (
       <div>
-        <Order />
-        <Order />
+        {orders}
       </div>
     )
   }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
