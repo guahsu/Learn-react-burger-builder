b12bf32ccfd93418b8f908f31a624ee60cf7b9df
GuaHsu <guaswork@gmail.com>
2019-02-10 22:11:40 +0800

    Fix validation issue

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,9 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: 'fastest'
+        value: 'fastest',
+        validation: {},
+        valid: true
       }
     },
     formIsValid: false,
